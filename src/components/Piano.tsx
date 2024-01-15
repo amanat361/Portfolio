638a9112096f98e17e778718a894f0bfcad35252
Samin Amanat <amanat361@gmail.com>
2024-01-14 23:29:25 -0800

    bug fixes and piano sizing modifications

--- src/components/Piano.tsx
+++ src/components/Piano.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as d3 from 'd3'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import useKeypress from 'react-use-keypress'
 
 type Note = {
@@ -24,14 +24,8 @@ const notes = [
 const randomNote = () => notes[Math.floor(Math.random() * notes.length)]
 
 export default function Piano() {
-  const [currentNote, setCurrentNote] = useState<Note>(randomNote())
-  
-  const keyColors = d3.scaleSequential([0,notes.length], currentNote.scheme)
-  
-  const pianoWidth = 100
-  const pianoHeight = 30
-  const noteWidth = pianoWidth / notes.length
-  const noteHeight = pianoHeight
+  const [currentNote, setCurrentNote] = useState<Note>(notes[0])
+  const [score, setScore] = useState(0)
 
   useKeypress(notes.map(note => note.letter), (event: React.KeyboardEvent) => {
     const note = notes.find(note => note.letter === event.key)
@@ -39,37 +33,55 @@ export default function Piano() {
   })
 
   const handleNotePress = (note: Note) => {
-    if (note === currentNote) setCurrentNote(randomNote())
+    if (note === currentNote) {
+      setCurrentNote(randomNote())
+      setScore(currentScore => currentScore + 1)
+    } else {
+      setScore(0)
+    }
   }
+  
+  const keyColors = d3.scaleSequential([0,notes.length], currentNote.scheme)
+  
+  const pianoWidth = 100
+  const pianoHeight = 10
+  const noteWidth = pianoWidth / notes.length
+  const noteHeight = pianoHeight
 
   return (
+    <React.Fragment>
+    <div className="flex flex-row justify-end m-2">
+      <h1 className="text-xl text-zinc-900 dark:text-zinc-300">Score: {score}</h1>
+    </div>
     <svg
       viewBox={`-1 -1 ${pianoWidth + 1} ${pianoHeight + 2}`}
       className="w-full rounded-2xl bg-zinc-900 shadow-lg dark:bg-black"
     >
       {notes.map((note, i) => (
-        <>
+        <g key={i}>
           <rect
-            key={i}
             x={i * noteWidth}
             rx={10 / notes.length}
             y="0"
             width={noteWidth - 1}
             height={noteHeight}
             fill={note === currentNote ? 'white' : keyColors(i)}
+            className={note === currentNote ? 'animate-pulse' : ''}
             onClick={() => handleNotePress(note)}
           />
           <text
+            className='hidden sm:block'
             x={i * noteWidth + (noteWidth - 1) / 2}
-            y={noteHeight * 0.9}
+            y={noteHeight / 2 + 2}
             textAnchor="middle"
             fill={note === currentNote ? 'black' : 'white'}
             fontSize={noteWidth / 2}
           >
             {note.letter.toUpperCase()}
           </text>
-        </>
+        </g>
       ))}
     </svg>
+    </React.Fragment>
   )
 }
