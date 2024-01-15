cdf0896fdb3b6c6f36ceab92f24815cd3554afce
Samin Amanat <amanat361@gmail.com>
2024-01-14 22:26:40 -0800

    piano now uses d3

--- src/components/Piano.tsx
+++ src/components/Piano.tsx
@@ -1,67 +1,37 @@
+'use client'
+
 import * as d3 from 'd3'
+import { useState } from 'react'
+import useKeypress from 'react-use-keypress'
 
-const pianoWidth = 141
-const pianoHeight = 42
-const whiteKeyWidth = 9
-const blackKeyWidth = 7
+export default function Piano() {
+  const [pressedKey, setPressedKey] = useState<string | null>(null)
+  
+  const notes = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+  const keyColors = d3.scaleSequential([0,notes.length], d3.interpolateCool)
+  const pianoWidth = 100
+  const pianoHeight = 30
+  const noteWidth = pianoWidth / notes.length
+  const noteHeight = pianoHeight
 
-function BlackKey({ x }: { x: number }) {
+
+  useKeypress(notes, (event: React.KeyboardEvent) => {
+    setPressedKey(event.key)
+  })
+
   return (
-    <rect
-      x={x-2}
-      y="0"
-      rx={1}
-      width={blackKeyWidth}
-      height={pianoHeight / 1.7}
-      className="fill-current text-zinc-900 hover:-translate-y-0.5 active:-translate-y-1 transition-transform"
-    />
-  )
-}
-
-function WhiteKey({ x }: { x: number }) {
-  return (
-    <rect
-      x={x}
-      y="1"
-      rx={1}
-      width={whiteKeyWidth}
-      height={pianoHeight - 2}
-      className="fill-current text-white hover:text-zinc-200 active:text-zinc-400 transition-colors"
-    />
-  )
-}
-
-export default function Octive() {
-  return (
-    <svg viewBox={`-1 0 ${pianoWidth} ${pianoHeight}`} className="w-full bg-zinc-900 rounded-lg p-1 shadow-lg">
-      <WhiteKey x={0} />
-      <WhiteKey x={10} />
-      <WhiteKey x={20} />
-      <WhiteKey x={30} />
-      <WhiteKey x={40} />
-      <WhiteKey x={50} />
-      <WhiteKey x={60} />
-      <WhiteKey x={70} />
-      <WhiteKey x={80} />
-      <WhiteKey x={90} />
-      <WhiteKey x={100} />
-      <WhiteKey x={110} />
-      <WhiteKey x={120} />
-      <WhiteKey x={130} />
-      <BlackKey x={8} />
-      <BlackKey x={18} />
-      <BlackKey x={38} />
-      <BlackKey x={48} />
-      <BlackKey x={58} />
-      <BlackKey x={78} />
-      <BlackKey x={88} />
-      <BlackKey x={108} />
-      <BlackKey x={118} />
-      <BlackKey x={128} />
+    <svg
+      viewBox={`-1 -1 ${pianoWidth + 1} ${pianoHeight + 2}`}
+      className="w-full rounded-2xl bg-zinc-900 shadow-lg dark:bg-black"
+    >
+      {notes.map((note, i) => (
+        <>
+          <rect key={i} x={i * noteWidth} rx="1" y="0" width={noteWidth - 1} height={noteHeight} fill={note === pressedKey ? "white" : keyColors(i)} />
+          <text x={i * noteWidth + ((noteWidth-1) / 2)} y={noteHeight * 0.9} textAnchor="middle" fill={note === pressedKey ? "black" : "white"} fontSize={noteWidth / 2}>
+            {note.toUpperCase()}
+          </text>
+        </>
+      ))}
     </svg>
   )
-}
-
-function Piano() {
-
 }
